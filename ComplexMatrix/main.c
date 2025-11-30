c6dd844d72ff8f757054d90b5b5ab522c5c10122
Ichiyou1922 <mogi2fruits.kazu@gmail.com>
2025-11-30 14:27:47 +0900

    add new program

--- ComplexMatrix/main.c
+++ ComplexMatrix/main.c
@@ -21,7 +21,7 @@ ComplexMatrix *create_matrix(int rows, int cols) {
   }
   new_matrix->rows = rows;
   new_matrix->cols = cols;
-  new_matrix->data = (double complex *)calloc(rows + cols, sizeof(double complex));
+  new_matrix->data = (double complex *)calloc(rows * cols, sizeof(double complex)); //行列の面積はrow * cols, rows + colsじゃない．
   if (new_matrix->data == NULL) {
     printf("Failed to allocate data memory\n");
     free(new_matrix);
@@ -35,6 +35,13 @@ void free_matrix(ComplexMatrix *m) {
     free(m->data);
   }
   free(m);
+}
+
+void free_vector(ComplexVector *v) {
+  if (v != NULL) {
+    free(v->data);
+  }
+  free(v);
 }
 
 ComplexVector *create_vector(int size) {
@@ -53,37 +60,45 @@ ComplexVector *create_vector(int size) {
 }
 
 double complex get_matrix_element(const ComplexMatrix *m, int row, int col) {
-  if (row < 0 || row > m->rows || col < 0 || col > m->cols) {
-    printf("Value Error");
+  if (row < 0 || row >= m->rows || col < 0 || col >= m->cols) {
+    printf("Value Error\n");
     exit(1);
   } 
   int k = row * m->cols + col;
-  double complex ret = m->data[k];
-  return ret;
+  double complex seq = m->data[k];
+  return seq;
 }
 
 void set_matrix_element(ComplexMatrix *m, int row, int col, double complex value) {
-  if (row < 0 || row > m->rows || col < 0 || col > m->cols) {
-    printf("Value Error");
+  if (row < 0 || row >= m->rows || col < 0 || col >= m->cols) {
+    printf("Value Error\n");
     exit(1);
   }
   int k = row * m->cols + col;
   m->data[k] = value;
 }
 
+void set_vector_element(ComplexVector *v, int index, double complex value) {
+  if (index < 0 || index >= v->size) {
+    printf("Value Error\n");
+    exit(1);
+  }
+  v->data[index] = value;
+}
+
 // 行列ベクトル積(Matrix-Vector Multiplication)
 ComplexVector *mat_vec_mult(const ComplexMatrix *A, const ComplexVector *x) {
   // y = Ax
-  ComplexVector *y = 0;
-  y->size = A->rows;
+  ComplexVector *y = create_vector(A->rows);
+  
   if (A->cols != x->size) {
     printf("Value Error\n");
     exit(1);
   }
 
-  for (int i = 0; i < A->rows; i++) {
-    for (int j = 0; j < A->cols; i++) {
-      y->data[i] = get_matrix_element(A, i, j) * x->data[j];
+  for (int i = 0; i < (A->rows); i++) {
+    for (int j = 0; j < (A->cols); j++) {
+      y->data[i] += get_matrix_element(A, i, j) * x->data[j];
     }
   }
   return y;
@@ -96,10 +111,17 @@ int main(void) {
   set_matrix_element(sigma, 1, 0, 1);
   set_matrix_element(sigma, 1, 1, 0);
 
-  ComplexVector *y = 0;
-  ComplexVector *ans = mat_vec_mult(sigma, y);
-  printf("%f+%fi, %f+%fi", creal(ans->data[0]), cimag(ans->data[0]), creal(ans->data[1]), cimag(ans->data[1]));
-  free(y);
+  ComplexVector *x = create_vector(sigma->rows);
+  set_vector_element(x, 0, 1);
+  set_vector_element(x, 1, 0);
+  ComplexVector *y; 
+  y = mat_vec_mult(sigma, x);
+  for (int index = 0; index < sigma->rows; index++) {
+    printf("%f+%fi\n", creal(y->data[index]), cimag(y->data[index]));
+  }
+
+  free_vector(y);
+  free_vector(x);
   free_matrix(sigma);
   return 0;
 
